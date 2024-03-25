b88221f5ec6a3fff536e6124e09c950f124d9c78
zanetti33 <lorenzo.zanetti5@studio.unibo.it>
2024-03-25 12:38:07 +0100

    abbiamo i costruttori

--- src/main/scala/tasks/adts/Ex2SchoolModel.scala
+++ src/main/scala/tasks/adts/Ex2SchoolModel.scala
@@ -19,6 +19,9 @@ object SchoolModel:
     type School
     type Teacher
     type Course
+    def teacher(name: String): Teacher
+    def course(name: String): Course
+    def school(): School
     extension (school: School)
       def addTeacher(name: String): School
       def addCourse(name: String): School
@@ -29,24 +32,30 @@ object SchoolModel:
       def setTeacherToCourse(teacher: Teacher, course: Course): School
       def coursesOfATeacher(teacher: Teacher): Sequence[Course]
 
-    object SchoolModuleImpl extends SchoolModule:
+  object SchoolModuleImpl extends SchoolModule:
 
-      private case class SchoolImpl(teachers: List[Teacher], courses: List[Course])
-      
-      opaque type School = SchoolImpl
+    private case class SchoolImpl(teachers: Sequence[Teacher], courses: Sequence[Course])
+    private case class TeacherImpl(name: String, courses: Sequence[Course])
+    private case class CoruseImpl(name: String)
+    opaque type School = SchoolImpl
+    opaque type Teacher = TeacherImpl
+    opaque type Course = CoruseImpl
+    def school(): School = SchoolImpl(Sequence.Nil(), Sequence.Nil())
+    def teacher(name: String): Teacher = TeacherImpl(name, Sequence.Nil())
+    def course(name: String): Course = CoruseImpl(name)
 
-      extension (school: School) override def addCourse(name: String): School = ???
+    extension (school: School) override def addCourse(name: String): School = ???
 
-      extension (school: School) override def teacherByName(name: String): Optional[Teacher] = ???
+    extension (school: School) override def teacherByName(name: String): Optional[Teacher] = ???
 
-      extension (school: School) override def addTeacher(name: String): School = ???
+    extension (school: School) override def addTeacher(name: String): School = ???
 
-      extension (school: School) override def coursesOfATeacher(teacher: Teacher): Sequence[Course] = ???
+    extension (school: School) override def coursesOfATeacher(teacher: Teacher): Sequence[Course] = ???
 
-      extension (school: School) override def setTeacherToCourse(teacher: Teacher, course: Course): School = ???
+    extension (school: School) override def setTeacherToCourse(teacher: Teacher, course: Course): School = ???
 
-      extension (school: School) override def nameOfTeacher(teacher: Teacher): String = ???
+    extension (school: School) override def nameOfTeacher(teacher: Teacher): String = ???
 
-      extension (school: School) override def nameOfCourse(teacher: Teacher): String = ???
+    extension (school: School) override def nameOfCourse(teacher: Teacher): String = ???
 
-      extension (school: School) override def courseByName(name: String): Optional[Course] = ???
+    extension (school: School) override def courseByName(name: String): Optional[Course] = ???
--- src/test/scala/tasks/adts/SchoolModelTest.scala
+++ src/test/scala/tasks/adts/SchoolModelTest.scala
@@ -2,24 +2,29 @@ package tasks.adts
 
 import org.junit.*
 import org.junit.Assert.*
-import tasks.adts.SchoolModel.SchoolModule
+import tasks.adts.SchoolModel.*
 import u02.AlgebraicDataTypes.Person
 import u03.Optionals.Optional
 import org.junit.jupiter.api.BeforeEach
+import u03.Optionals.Optional.orElse
 
 class SchoolModelTest {
 
     // Choice of implementation to test
-  val schoolModelADT: SchoolModule = SchoolModuleImpl
-  import schoolModelADT.*
+  val schoolModule: SchoolModule = SchoolModuleImpl
+  import schoolModule.*
 
-  @BeforeEach def initializeSchool() =
-    val school = school()
+  var unibo: School = school()
+
+  @BeforeEach def resetSchool() =
+    unibo = school()
 
   // From now, everything is independent of specific implementation of Complex
 
   @Test def teachersCanBeAdded() =
-    assertEquals(Optional.Empty, school teacherByName "Viroli")
+    assertEquals(Optional.Empty, unibo teacherByName "Viroli")
+    unibo addTeacher "Viroli"
+    assertEquals(Optional.Just(Teacher), unibo nameOfTeacher (orElse(unibo teacherByName "Viroli")))
     
 
 }
