340234a4175ac812486fe85cb9bdc27ac0ffe9a9
zanetti33 <lorenzo.zanetti5@studio.unibo.it>
2024-04-15 19:22:08 +0200

    completed school adt

--- src/main/scala/tasks/adts/Ex2SchoolModel.scala
+++ src/main/scala/tasks/adts/Ex2SchoolModel.scala
@@ -2,8 +2,7 @@ package tasks.adts
 import u03.Sequences.*
 import u03.Optionals.*
 import u02.AlgebraicDataTypes.Person
-import u03.Sequences.Sequence.filter
-import u03.Sequences.Sequence.findFirst
+import scala.collection.immutable.Stream.Cons
 
 /*  Exercise 2: 
  *  Implement the below trait, and write a meaningful test.
@@ -30,7 +29,7 @@ object SchoolModel:
       def teacherByName(name: String): Optional[Teacher]
       def courseByName(name: String): Optional[Course]
       def nameOfTeacher(teacher: Teacher): String
-      def nameOfCourse(teacher: Teacher): String
+      def nameOfCourse(course: Course): String
       def setTeacherToCourse(teacher: Teacher, course: Course): School
       def coursesOfATeacher(teacher: Teacher): Sequence[Course]
 
@@ -38,31 +37,46 @@ object SchoolModel:
 
     private case class SchoolImpl(teachers: Sequence[Teacher], courses: Sequence[Course])
     private case class TeacherImpl(name: String, courses: Sequence[Course])
-    private case class CoruseImpl(name: String)
+    private case class CourseImpl(name: String)
     opaque type School = SchoolImpl
     opaque type Teacher = TeacherImpl
-    opaque type Course = CoruseImpl
+    opaque type Course = CourseImpl
     def schoolImpl(): School = SchoolImpl(Sequence.Nil(), Sequence.Nil())
-    private def schoolImpl(teacher: Sequence[Teacher])(courses: Sequence[Course]) = SchoolImpl(teacher, courses)
+    private def schoolImpl(teachers: Sequence[Teacher])(courses: Sequence[Course]) = SchoolImpl(teachers, courses)
     def teacher(name: String): Teacher = TeacherImpl(name, Sequence.Nil())
-    def course(name: String): Course = CoruseImpl(name)
+    def course(name: String): Course = CourseImpl(name)
 
-    extension (school: School) override def addCourse(name: String): School = ???
+    extension (school: School) override def addCourse(name: String): School = 
+      schoolImpl(school.teachers)(Sequence.Cons(CourseImpl(name), school.courses))
 
-    extension (school: School) override def teacherByName(name: String): Optional[Teacher] = ???
+    extension (school: School) override def teacherByName(name: String): Optional[Teacher] = 
+      Sequence.findFirst(school.teachers)(_.name == name)
 
-    extension (school: School) override def addTeacher(name: String): School = schoolImpl
-      (school.teachers match
-        case Sequence.Cons(head, tail) => Sequence.Cons(teacher(name), Sequence.Cons(head, tail))
-        case Sequence.Nil() => Sequence.Cons(teacher(name), Sequence.Nil()))
-      (school.courses)
+    extension (school: School) override def addTeacher(name: String): School = 
+      schoolImpl(Sequence.Cons(TeacherImpl(name, Sequence.Nil()), school.teachers))(school.courses)
 
-    extension (school: School) override def coursesOfATeacher(teacher: Teacher): Sequence[Course] = ???
+    extension (school: School) override def coursesOfATeacher(teacher: Teacher): Sequence[Course] =
+      Sequence.findFirst(school.teachers)(_.name == teacher.name) match
+        case Optional.Just(a) => a.courses
+        case Optional.Empty() => Sequence.Nil()
 
-    extension (school: School) override def setTeacherToCourse(teacher: Teacher, course: Course): School = ???
-
+    extension (school: School) override def setTeacherToCourse(teacher: Teacher, course: Course): School =
+      schoolImpl(Sequence.map[Teacher, Teacher](school.teachers)(t => t match
+        case x if x.name == teacher.name => TeacherImpl(t.name, Sequence.Cons(course, t.courses))
+        case _ => t)
+      )(school.courses)
+      
+      
     extension (school: School) override def nameOfTeacher(teacher: Teacher): String = teacher.name
 
-    extension (school: School) override def nameOfCourse(teacher: Teacher): String = ???
+    extension (school: School) override def nameOfCourse(course: Course): String = course.name
 
-    extension (school: School) override def courseByName(name: String): Optional[Course] = ???
+    extension (school: School) override def courseByName(name: String): Optional[Course] = 
+      Sequence.findFirst(school.courses)(_.name == name)
+    
+@main def trySchool =
+  import SchoolModel.SchoolModuleImpl.*
+  var unibo: School = schoolImpl()
+  val newUnibo = unibo addTeacher "Viroli" setTeacherToCourse(teacher("Viroli"), course("PPS"))
+  println(unibo addTeacher "Viroli" setTeacherToCourse(teacher("Viroli"), course("PPS")) coursesOfATeacher teacher("Viroli"))
+
--- src/test/scala/tasks/adts/SchoolModelTest.scala
+++ src/test/scala/tasks/adts/SchoolModelTest.scala
@@ -5,6 +5,8 @@ import org.junit.Assert.*
 import tasks.adts.SchoolModel.*
 import u02.AlgebraicDataTypes.Person
 import u03.Optionals.Optional
+import u03.Sequences.*
+import u03.Optionals.*
 import org.junit.jupiter.api.BeforeEach
 import u03.Optionals.Optional.orElse
 import scala.collection.View.Empty
@@ -22,9 +24,13 @@ class SchoolModelTest {
   
     @Test def schoolIsInitiallyEmpty() =
         assertEquals(Optional.Empty(), unibo teacherByName "Viroli")
+  
+    @Test def coursesCanBeAdded() =
+        assertEquals(Optional.Just(course("PPS")), unibo addCourse "PPS" courseByName "PPS")
 
     @Test def teachersCanBeAdded() =
         assertEquals(Optional.Just(teacher("Viroli")), unibo addTeacher "Viroli" teacherByName "Viroli")
     
-
+    @Test def coursesCanBeSetToTeachers() =
+        assertEquals(Sequence.Cons(course("PPS"), Sequence.Nil()), unibo addTeacher "Viroli" setTeacherToCourse(teacher("Viroli"), course("PPS")) coursesOfATeacher teacher("Viroli"))
 }
